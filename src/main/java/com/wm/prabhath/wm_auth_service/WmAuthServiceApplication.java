46245c7cebda84100117ce27a33c51194a2cd8e6
anuradha <anuradha.kadurugasyaya@gmail.com>
2024-07-06 10:33:17 +0800

    first commit

---
+++ src/main/java/com/wm/prabhath/wm_auth_service/WmAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wm.prabhath.wm_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmAuthServiceApplication.class, args);
+	}
+
+}
